34a2630c5529181d56ad4ea0771446eec18ffa11
Tiago <tiagoversuti@gmail.com>
2016-10-03 17:03:50 -0300

    Módulos: 1 - Introdução a arquitetura MVC 2 - Estrutura do projeto MVC 5 3 - O que é um Controller?

---
+++ Asp.NetMvc5ConceitosBasicos/Asp.NetMvc5ConceitosBasicos/Controllers/OlaMundoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp.NetMvc5ConceitosBasicos.Controllers
+{
+    public class OlaMundoController : Controller
+    {
+        // GET: OlaMundo
+        public string Index()
+        {
+            return "Olá MVC 5";
+        }
+
+        // GET: OlaMundo/QueridoController
+        public string QueridoController()
+        {
+            return "Texto <b>em negrito</b>, retorno do controller";
+        }
+
+        // GET: OlaMundo/GetJson
+        public JsonResult GetJson()
+        {
+            var produto = new
+            {
+                ID = 12,
+                Nome = "Bicicleta",
+                Qtde = 10
+            };
+
+            return Json(produto, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: OlaMundo/GetDados?nome=Renato&idade=45
+        public string GetDados(string nome, int idade = 20)
+        {
+            return HttpUtility.HtmlEncode(
+                string.Format("Bem vindo {0} com idade {1}",
+                                    nome, idade));
+        }
+    }
+}
---
+++ Asp.NetMvc5ConceitosBasicos/Asp.NetMvc5ConceitosBasicos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Asp.NetMvc5ConceitosBasicos.Startup))]
+namespace Asp.NetMvc5ConceitosBasicos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
