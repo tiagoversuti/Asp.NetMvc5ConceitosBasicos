c1d4836a7ae88db61b3edd69165b767bedfdaec8
tiago <tiago@TIAGO-NB>
2016-10-03 22:19:42 -0300

    Módulos: 4 - Model e Controller - parte 1 5 - Model e Controller - parte 2

---
+++ Asp.NetMvc5ConceitosBasicos/Asp.NetMvc5ConceitosBasicos/Controllers/LivrosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Asp.NetMvc5ConceitosBasicos.Models;
+
+namespace Asp.NetMvc5ConceitosBasicos.Controllers
+{
+    public class LivrosController : Controller
+    {
+        private BancoContexto db = new BancoContexto();
+
+        // GET: Livros
+        public ActionResult Index()
+        {
+            return View(db.Livroes.ToList());
+        }
+
+        // GET: Livros/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Livro livro = db.Livroes.Find(id);
+            if (livro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(livro);
+        }
+
+        // GET: Livros/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Livros/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDLivro,Titulo,Autor,Preco,Estoque,Disponivel")] Livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Livroes.Add(livro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(livro);
+        }
+
+        // GET: Livros/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Livro livro = db.Livroes.Find(id);
+            if (livro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(livro);
+        }
+
+        // POST: Livros/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDLivro,Titulo,Autor,Preco,Estoque,Disponivel")] Livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(livro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(livro);
+        }
+
+        // GET: Livros/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Livro livro = db.Livroes.Find(id);
+            if (livro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(livro);
+        }
+
+        // POST: Livros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Livro livro = db.Livroes.Find(id);
+            db.Livroes.Remove(livro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Asp.NetMvc5ConceitosBasicos/Asp.NetMvc5ConceitosBasicos/Models/Livro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Asp.NetMvc5ConceitosBasicos.Models
+{
+    public class Livro
+    {
+        [Key]
+        public int IDLivro { get; set; }
+
+        [Required(ErrorMessage = "Digite o título do livro.")]
+        [StringLength(100, ErrorMessage = "O tamanho máximo de caracteres é 100", MinimumLength = 6)]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Digite o autor do livro.")]
+        public string Autor { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int Estoque { get; set; }
+
+        public bool Disponivel { get; set; }
+
+    }
+}
